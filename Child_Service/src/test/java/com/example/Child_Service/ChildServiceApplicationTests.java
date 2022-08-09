fe4515ab7ff02e000edb62f26701fffe5c312315
mindc1jun109 <mindc1jun109@c1jun48.iiht.tech>
2022-08-09 15:08:31 +0530

    Child

---
+++ Child_Service/src/test/java/com/example/Child_Service/ChildServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Child_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
